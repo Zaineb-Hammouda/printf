2a374b7e9633e570882691e3cd6dbba369d5a566
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-03-27 00:30:54 +0200

    the printf function

---
+++ _printf.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _printf - prints formatted text to stdout
+ * Return: number of printed chars or -1 if it fails
+ * @format: string input
+ */
+int _printf(const char *format, ...)
+{
+	int printed_chars;
+	s_printf func_arr[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"%", print_percent},
+		{"d", print_int},
+		{"i", print_int},
+		{NULL, NULL}
+	};
+
+	va_list arg_list;
+
+	if (format == NULL)
+		return (-1);
+	va_start(arg_list, format);
+	printed_chars = select_f(format, func_arr, arg_list);
+	va_end(arg_list);
+	return (printed_chars);
+}
