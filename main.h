2e72fc0532c4b48ae1e6e100634f7ce11500404d
Zaineb-Hammouda <122837330+Zaineb-Hammouda@users.noreply.github.com>
2023-03-27 16:11:18 +0200

    updated main file

--- main.h
+++ main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
 
 /**
  * struct s_printf - defines a structure for symbols and functions
@@ -17,15 +18,29 @@ typedef struct s_printf
 } s_printf;
 
 int _printf(const char *format, ...);
+int select_f(const char *format, s_printf func_arr[], va_list arg_list);
 
 int print_char(va_list);
 int print_num(long int n);
 int print_int(va_list);
 int print_str(va_list);
 int print_percent(va_list);
+int print_binary(va_list);
+int print_reversed(va_list arg);
+int rot13(va_list);
+int unsigned_integer(va_list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_heX(va_list list);
+
+/*Helping functions*/
+unsigned int base_len(unsigned int, int);
+char *rev_string(char *);
+void write_base(char *str);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int print_unsgined_number(unsigned int);
 
 int _write_char(char);
 
-int select_f(const char *format, s_printf func_arr[], va_list arg_list);
 
 #endif
